400e829dd41aba17af235ec10d379a8914769ae5
Vitor Cimetan de Souza <vitaocimetan@gmail.com>
2019-05-02 16:32:05 -0300

    Uso da bib gurobi adicionada

--- mix.py
+++ mix.py
@@ -1,13 +1,45 @@
 # -*- coding: latin-1 -*-
+from gurobipy import *
 
 class Produto:
     def __init__(self, produto):
         self.produto = produto
         self.quantidade = []
+        self.preco = 0
     def nome_produto(self):
         return self.produto
     def set_quantidade(self, quantidade):
-        self.quantidade = quantidade
+        self.quantidade.append(quantidade)
+    def set_preco(self, preco):
+        self.preco = preco
+    def get_preco(self):
+        return self.preco
+    def get_quantidade(self, index):
+        return self.quantidade[index]
+
+def mixproducao(num_produtos, num_recursos, produtos, disp_rec):
+    m = Model("mixproducao")
+    variaveis = []
+    obj_funcao = LinExpr()
+    for i in range(0, num_produtos):
+        variaveis.append(m.addVar(vtype = GRB.INTEGER , name = produtos[i].nome_produto()))
+    
+    for v in m.getVars():
+        obj_funcao += (v * produtos[i].get_preco())
+    m.setObjective(obj_funcao, GRB.MAXIMIZE)
+    
+    for y in range(0, num_recursos):
+        obj_restricao = LinExpr()
+        i = 0
+        for v in m.getVars():
+            obj_restricao += (v * produtos[i].get_quantidade(y))
+            i += 1
+        m.addConstr(obj_restricao, GRB.LESS_EQUAL, disp_rec[y], "c" + str(y))
+    m.optimize()
+    for v in m.getVars():
+        print('%s %g' % (v.varName, v.x))
+
+    print('Obj: %g' % m.objVal)
 
 def main():
    num_produtos = input("Digite o numero de produtos: ")
@@ -22,13 +54,14 @@ def main():
    for i in range(0, num_recursos):
        nome_rec.append(raw_input("Qual o nome do recurso "  +str(i+1) + ": ")) 
    for i in range(0, num_produtos):
-       preco_prod.append(raw_input("Quanto é recebido pelo produto " + (produtos[i].nome_produto()) + ": "))          
+       produtos[i].set_preco(input("Quanto é recebido pelo produto " + (produtos[i].nome_produto()) + ": "))          
    for i in range(0, num_recursos):
-       disp_rec.append(raw_input("Quanto de " + nome_rec[i] + " está dísponínel: ")) 
+       disp_rec.append(input("Quanto de " + nome_rec[i] + " está dísponínel: ")) 
    for i in range(0, num_produtos):
        for y in range(0, num_recursos):
-              produtos[i].set_quantidade(raw_input("Quanto de " + nome_rec[y] + " é usado em " + produtos[i].nome_produto()))
-   for i in range(0, num_produtos):
-       print(produtos[i].nome_produto())         
+              produtos[i].set_quantidade(input("Quanto de " + nome_rec[y] + " é usado em " + produtos[i].nome_produto() + " :"))
+        
+   mixproducao(num_produtos, num_recursos, produtos, disp_rec)
+
 if __name__ == "__main__":
     main()
